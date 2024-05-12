463d88d2f7c70df255090995e1277d5bc84f61f1
samisalehsaeed <samisalehsaeed05@gmail.com>
2024-05-12 13:19:15 +0100

    Sami Saeed

--- src/components/AnswersItem.jsx
+++ src/components/AnswersItem.jsx
@@ -5,7 +5,7 @@ const answersSet = {
   swimming: "Swimming",
   bathing: "Bathing",
   chatting: "Chatting",
-  noTime: "I don't like to spend time with it"
+  noTime: "I don't like to spend time with it",
 };
 
 function ItemsList({ list }) {
@@ -22,7 +22,7 @@ function ItemsList({ list }) {
 export default function AnswersItem({
   // Feel free to change this props names to what suits you best
   // Rememeber here we're destructuring answerItem, which is the prop name that we've passed
-  answerItem: { username, colour, timeSpent, review }
+  answerItem: { username, colour, timeSpent, review },
 }) {
   return (
     <li>
--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,17 +1,142 @@
 import { useState } from "react";
 
-function Survey() {
-  const [open, setOpen] = useState(false); //Ignore this state
+export default function Survey() {
+  const [open, setOpen] = useState(false); //Ignore
+  const [formData, setFormData] = useState({
+    color: "",
+    "spend-time": [],
+    review: "",
+    username: "",
+    email: "",
+  });
+  const [answers, setAnswers] = useState([]);
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]:
+        type === "checkbox"
+          ? checked
+            ? [...prevFormData[name], value]
+            : prevFormData[name].filter((item) => item !== value)
+          : value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setAnswers((prevAnswers) => [...prevAnswers, formData]);
+    setFormData({
+      color: "",
+      "spend-time": [],
+      review: "",
+      username: "",
+      email: "",
+    });
+  };
 
   return (
     <main className="survey">
       <section className={`survey__list ${open ? "open" : ""}`}>
         <h2>Answers list</h2>
         {/* answers should go here */}
+        {/* Display Answers */}
+        {answers.map((answer, index) => (
+          <div key={index}>
+            <p>I rate my rubber duck color a: {answer.color}</p>
+            <br />
+            <p> Spend Time by: {answer["spend-time"].join(", ")}</p>
+            <br />
+            <p>Review: {answer.review}</p>
+            <br />
+            <p>Name: {answer.username}</p>
+            <br />
+            <p>Email: {answer.email}</p>
+          </div>
+        ))}
       </section>
-      <section className="survey__form">{/* a form should be here */}</section>
+      <section className="survey__form">
+        <form className="form" onSubmit={handleSubmit}>
+          {/* Questions */}
+          <h2>Tell us what you think about your rubber duck!</h2>
+          <div className="form__group radio">
+            <h3>How do you rate your rubber duck colour?</h3>
+            <ul>
+              {[...Array(4)].map((_, index) => (
+                <li key={index}>
+                  <input
+                    id={`color-${index + 1}`}
+                    type="radio"
+                    name="color"
+                    value={index + 1}
+                    checked={formData.color === (index + 1).toString()}
+                    onChange={handleChange}
+                  />
+                  <label htmlFor={`color-${index + 1}`}>{index + 1}</label>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="form__group">
+            <h3>How do you like to spend time with your rubber duck</h3>
+            <ul>
+              {[
+                "Swimming",
+                "Bathing",
+                "Chatting",
+                "I don't like to spend time with it",
+              ].map((option) => (
+                <li key={option}>
+                  <label>
+                    <input
+                      name="spend-time"
+                      type="checkbox"
+                      value={option}
+                      checked={formData["spend-time"].includes(option)}
+                      onChange={handleChange}
+                    />
+                    {option}
+                  </label>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <label>
+            What else have you got to say about your rubber duck?
+            <textarea
+              name="review"
+              cols="30"
+              rows="10"
+              value={formData.review}
+              onChange={handleChange}
+            ></textarea>
+          </label>
+          <label>
+            Put your name here (if you feel like it):
+            <input
+              type="text"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Leave us your email pretty please??
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+            />
+          </label>
+          <input
+            className="form__submit"
+            type="submit"
+            value="Submit Survey!"
+          />
+        </form>
+      </section>
     </main>
   );
 }
-
-export default Survey;
